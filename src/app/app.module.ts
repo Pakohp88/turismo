02edd61878661496be21736904b4363a1d4728a8
Francisco Hernandez Perez <franciscohernandez@lavore.com.mx>
2021-11-21 19:18:24 -0600

    se agregaron componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { EstadosComponent } from './components/estados/estados.component';
+import { EstadoComponent } from './components/estado/estado.component';
+import { EstadoCardComponent } from './components/estado-card/estado-card.component';
+import { SearchComponent } from './components/search/search.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    EstadosComponent,
+    EstadoComponent,
+    EstadoCardComponent,
+    SearchComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/estado-card/estado-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-estado-card',
+  templateUrl: './estado-card.component.html',
+  styleUrls: ['./estado-card.component.css']
+})
+export class EstadoCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
